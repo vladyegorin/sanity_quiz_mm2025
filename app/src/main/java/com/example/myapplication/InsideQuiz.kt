29938c17aaac38fa9f1a030f98b017e1422019fd
vova <vladyegorin@gmail.com>
2025-03-08 13:40:16 -0800

    quiz layout

--- app/src/main/java/com/example/myapplication/InsideQuiz.kt
+++ app/src/main/java/com/example/myapplication/InsideQuiz.kt
@@ -1,9 +1,13 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class InsideQuiz: AppCompatActivity() {
@@ -12,18 +16,45 @@ class InsideQuiz: AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.insidequiz)
         val panicButton = findViewById<Button>(R.id.panicButton)
+        var questionCounter = 0;
+
         //30+
         val pulseAnimation = AnimationUtils.loadAnimation(this, R.anim.pulsation_animation)
+
         //60+
         val pulseAnimationExtra = AnimationUtils.loadAnimation(this, R.anim.pulsation_animation_extra)
+
         //100
         val pulseAnimation100 = AnimationUtils.loadAnimation(this, R.anim.pulsation_animation_100)
-        panicButton.startAnimation(pulseAnimation)
+
+        panicButton.visibility = Button.VISIBLE;
+        //panicButton.startAnimation(pulseAnimation)
         panicButton.setOnClickListener {
-            panicButton.startAnimation(pulseAnimation100)
+            noNeedToPanicDialog()
         }
 
 
 
+    }
+
+    private fun noNeedToPanicDialog() {
+
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.no_need_to_panic_dialogue, null)
+
+
+        val dialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .create()
+
+
+        val okButton: Button = dialogView.findViewById(R.id.okButton)
+
+
+        okButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+
+        dialog.show()
     }
 }
